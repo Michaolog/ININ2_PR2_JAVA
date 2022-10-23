b99b156e1daca3dddd887644d6a9a620717e8da6
Michał Łuczak <michalluczak1906@gmail.com>
2022-10-23 14:13:20 +0200

    Zadanie 0,1,2,3/4

--- src/Human.java
+++ src/Human.java
@@ -1,18 +1,30 @@
+import java.util.Date;
 public class Human {
     String firstName;
     String lastName;
     final Integer yearOfBirth;
     Animal pet;
     Car car;
-    private Double salary;
-    private Double lastSalary;
+    private Double salary = 0.0;
+    private Double lastSalary = 0.0;
+    private Date lastSalaryInfo;
+
 
     Human(Integer yearOfBirth) {
         this.yearOfBirth = yearOfBirth;
     }
 
-    public void getSalary(){
-        this.salary;
+
+    public Double getSalary(){
+        if(lastSalaryInfo != null) {
+            System.out.println("Informację o twoim wynagrodzeniu ostatnio pobierano: " + lastSalaryInfo);
+        }
+        else {
+            System.out.println("Informację o twoim wynagrodzeniu ostatnio pobierano: nigdy");
+        }
+        System.out.println("Jego wysokość wynosiła wtedy: " + this.lastSalary);
+        this.lastSalaryInfo = new Date();
+        return this.salary;
     }
     public void setSalary(Double salary) {
         if(salary >= 0 && !this.salary.equals(salary)) {
@@ -20,7 +32,7 @@ public class Human {
             this.salary = salary;
             System.out.println("Informacja o nowej wysokości wynagrodzenia została odnotowana w systemie księgowym!");
             System.out.println("Proszę odebrać aneks do umowy w dziale kadr!");
-            System.out.println("ZUS i US już wiedzą ;) Nie ukrywaj nowego dochodu!");
+            System.out.println("ZUS i US już wiedzą ;) Nie ukrywaj nowego dochodu!\n");
         }
         else if(this.salary.equals(salary)){
             System.out.println("Wysokość wynagrodzenia jest już na podanym poziomie!!");
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        //Zadanie 1
         Human me = new Human(2000);
         me.firstName = "Michał";
         me.lastName = "Łuczak";
@@ -9,22 +12,24 @@ public class Main {
 
         System.out.println("Obecna waga " + me.pet.name + "a wynosi: " + me.pet.getWeight() + ".");
         me.pet.feed();
-        System.out.println("Obecna waga " + me.pet.name + "a wynosi: " + me.pet.getWeight() + ".");
-        me.pet.takeForAWalk();
-        System.out.println("Obecna waga " + me.pet.name + "a wynosi: " + me.pet.getWeight() + ".");
-        me.pet.takeForAWalk();
-        System.out.println("Obecna waga " + me.pet.name + "a wynosi: " + me.pet.getWeight() + ".");
-        me.pet.takeForAWalk();
-        System.out.println("Obecna waga " + me.pet.name + "a wynosi: " + me.pet.getWeight() + ".");
-        me.pet.takeForAWalk();
-        System.out.println("Obecna waga " + me.pet.name + "a wynosi: " + me.pet.getWeight() + ".");
-        me.pet.takeForAWalk();
+        for(int i = 0; i < 5; i++) {
+            System.out.println("Obecna waga " + me.pet.name + "a wynosi: " + me.pet.getWeight() + ".");
+            me.pet.takeForAWalk();
+        }
         me.pet.takeForAWalk();
         me.pet.feed();
 
-
+        //Zadanie 2
         me.car = new Car("FSO", "Caro Plus");
         me.car.color = "czerwony";
         me.car.millage = 100000;
+
+        //Zadanie 3 i 4
+        Scanner scan = new Scanner(System.in);
+        for(int i = 0; i < 3; i++) {
+            System.out.print("\nPodaj wysokość nowego wynagrodznia dla " + me.firstName + " " + me.lastName + ": ");
+            me.setSalary(Double.parseDouble(scan.nextLine()));
+            System.out.println("Twoje obecne wynagrodzenie wynosi:" + me.getSalary());
+        }
     }
 }
